80c0775d636ade8656ebc6246abb634959161d7f
高橋啓太 <Keita@takahashikeita-no-iMac.local>
2017-05-17 22:40:53 +0900

    task(3)

--- no5/algo5.c
+++ no5/algo5.c
@@ -1,89 +1,111 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX 10
 
-typedef struct {
+typedef struct{
+  char name[20];
+  int height;
+  double vision;
+} PhysCheck;
+
+typedef struct{
   int max;
   int ptr;
-  int stk[MAX];
-} IntStack;
+  PhysCheck stk[MAX];
+} Stack;
 
-int Initialize(IntStack *s,int max){
+int Initialize(Stack *s,int max){
   s->ptr = 0;
   s->max = max;
   return 0;
 }
 
-int Push(IntStack *s,int x){
+int Push(Stack *s,char *n,int h,double v){
   if(s->ptr >= s->max) return -1;
-  s->stk[s->ptr] = x;
+  strcpy(s->stk[s->ptr].name, n);
+  //s->stk[s->ptr].name = n;
+  s->stk[s->ptr].height = h;
+  s->stk[s->ptr].vision = v;
   s->ptr++;
   return 0;
 }
 
-int Pop(IntStack *s,int *x){
+int Pop(Stack *s,char *n,int *h,double *v){
   if(s->ptr <= 0)return -1;
   s->ptr--;
-  *x = s->stk[s->ptr];
+  strcpy(n,s->stk[s->ptr].name);
+  //n = s->stk[s->ptr -1].name;
+  *h = s->stk[s->ptr].height;
+  *v = s->stk[s->ptr].vision;
   return (0);
 }
 
-int Peek(IntStack *s,int *x){
+int Peek(Stack *s,char *n,int *h,double *v){
   if(s->ptr <= 0)return -1;
-  *x = s->stk[s->ptr -1];
+  n = s->stk[s->ptr -1].name;
+  *h = s->stk[s->ptr -1].height;
+  *v = s->stk[s->ptr -1].vision;
   return 0;
 }
 
-int Capacity(const IntStack *s){
+int Capacity(const Stack *s){
   return s->max;
 }
 
-int Size(const IntStack *s){
+int Size(const Stack *s){
   return s->ptr;
 }
 
-void Print(const IntStack *s){
+void Print(const Stack *s){
   int i;
   for(i = 0;i< s->ptr;i++)
-    printf("%d",s->stk[i]);
+    printf("%s %d %lf\n",s->stk[i].name,s->stk[i].height,s->stk[i].vision);
   putchar('\n');
 }
 
 int main(void){
-  IntStack s;
+  Stack s;
   Initialize(&s,MAX);
 
   while(1){
-    int menu,x;
+    int menu;
+    char n[20];
+    int h;
+    double v;
     printf("現在のデータ数:%d/%d\n",Size(&s),Capacity(&s));
-    printf("(1)プッシュ (2)ポップ (3)ピーク (4)表示 (0)終了");
+    printf("(1)プッシュ (2)ポップ (3)ピーク (4)表示 (0)終了:");
     scanf("%d",&menu);
     
     if(menu==0) break;
 
     switch(menu){
       case 1:
-       printf("データ:");
-       scanf("%d",&x);
-       if(Push(&s,x)==-1)
+       printf("Name:");
+       scanf("%s",n);
+       printf("Height:");
+       scanf("%d",&h);
+       printf("Vision:");
+       scanf("%lf",&v);
+       if(Push(&s,n,h,v)==-1)
          puts("¥aエラー:プッシュに失敗しました。");
        break;
       case 2:
-       if(Pop(&s,&x)==-1)
-         puts("¥aエラー:ポップに失敗しました。");
+       if(Pop(&s,n,&h,&v)==-1)
+         puts("\aエラー:ポップに失敗しました。");
        else
-         printf("ポップしたデータは%dです。¥n",x);
+         printf("ポップしたデータは%s,%d,%lfです。\n",n,h,v);
        break;
       case 3:
-       if(Peek(&s,&x)==-1)
-         puts("¥aエラー:ピークに失敗しました。");
+       if(Peek(&s,n,&h,&v)==-1)
+         puts("\aエラー:ピークに失敗しました。");
        else
-         printf("ピークしたデータは%dです。¥n",x);
+         printf("ピークしたデータは%s,%d,%lfです。\n",n,h,v);
        break;
       case 4:
        Print(&s);
        break;
-
     }
   }
   return 0;
 }
+
