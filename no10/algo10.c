ac28862d750a9a67753072d2017cbc1a8ea58337
Keita <bonjovi1926@yahoo.co.jp>
2017-06-19 09:55:22 +0900

    no10

---
+++ no10/algo10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+#define String_Max 82
+
+void recur2(char *st){
+   int n = strlen(st)
+   if(n>0){
+       recur2(st+1);
+       if(n>1)recur2(st+2);
+       else recur2(st+1);
+       printf("%c",*st);
+   } 
+}
+
+void main(void){
+    char x[String_Max];
+
+    printf("文字列を入力せよ：");
+    scanf("%s",x);
+    recur2(x);
+    printf("\n");
+
+    return(0);
+}
